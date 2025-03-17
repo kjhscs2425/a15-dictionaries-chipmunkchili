06488d37cfec1c66bdb1c8eb5949b63e4dd66d1f
Stella Talcott <stalcott2027@kehillahstudent.org>
2025-03-17 09:21:07 -0700

    completed a15!

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+
+counter = {}
+
+text_lower = text.lower()
+
+for character in text_lower:
+    if character == "\n" or character == " " or character == "." or character == "'":
+            text_lower.replace(character, "")
+    elif character in counter:
+        counter[character] += 1
+    else:
+        counter[character] = 1
+        
+print(counter)
